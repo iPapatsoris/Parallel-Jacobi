cab4d5c07923b8d19bb253e8178b9f564baf56b9
Ioannis <giannhs95@yahoo.gr>
2021-01-21 16:32:57 +0200

    Optimize serial program

--- seqJacobi/jacobi_serial.c
+++ seqJacobi/jacobi_serial.c
@@ -45,7 +45,7 @@
  * NOTE: u(0,*), u(maxXCount-1,*), u(*,0) and u(*,maxYCount-1)
  * are BOUNDARIES and therefore not part of the solution.
  *************************************************************/
- double one_jacobi_iteration(double xStart, double yStart,
+inline __attribute__((always_inline)) double one_jacobi_iteration(double xStart, double yStart,
                             int maxXCount, int maxYCount,
                             double *src, double *dst,
                             double deltaX, double deltaY,
@@ -54,7 +54,7 @@
 #define SRC(XX,YY) src[(YY)*maxXCount+(XX)]
 #define DST(XX,YY) dst[(YY)*maxXCount+(XX)]
     int x, y;
-    double fX, fY;
+    double fX, fY, fXSquare, fYSquare;
     double error = 0.0;
     double updateVal;
     double f;
@@ -65,16 +65,17 @@
 
     for (y = 1; y < (maxYCount-1); y++)
     {
-        fY = yStart + (y-1)*deltaY;
+        fYSquare = (yStart + (y-1)*deltaY) * (yStart + (y-1)*deltaY);
         for (x = 1; x < (maxXCount-1); x++)
         {
-            fX = xStart + (x-1)*deltaX;
-            f = -alpha*(1.0-fX*fX)*(1.0-fY*fY) - 2.0*(1.0-fX*fX) - 2.0*(1.0-fY*fY);
+			fXSquare = (xStart + (x-1)*deltaX) * (xStart + (x-1)*deltaX);
+            f = -alpha*(1.0-fXSquare)*(1.0-fYSquare) - 2.0*(1.0-fXSquare) - 2.0*(1.0-fYSquare);
+			double curVal = SRC(x,y);
             updateVal = (	(SRC(x-1,y) + SRC(x+1,y))*cx +
                 			(SRC(x,y-1) + SRC(x,y+1))*cy +
-                			SRC(x,y)*cc - f
+                			curVal*cc - f
 						)/cc;
-            DST(x,y) = SRC(x,y) - omega*updateVal;
+            DST(x,y) = curVal - omega*updateVal;
             error += updateVal*updateVal;
         }
     }
@@ -180,6 +181,8 @@ int main(int argc, char **argv)
         u_old = u;
         u = tmp;
     }
+	int maxXcount = n+1;
+	int maxYcount = m+2;
 
     t2 = MPI_Wtime();
     printf( "Iterations=%3d Elapsed MPI Wall time is %f\n", iterationCount, t2 - t1 ); 
@@ -193,7 +196,7 @@ int main(int argc, char **argv)
 
     // u_old holds the solution after the most recent buffers swap
     double absoluteError = checkSolution(xLeft, yBottom,
-                                         n+2, m+2,
+                                         maxXcount, maxYcount,
                                          u_old,
                                          deltaX, deltaY,
                                          alpha);
